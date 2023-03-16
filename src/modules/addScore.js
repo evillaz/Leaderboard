87e6a51a2da3a7ba73c45ac505d17ca9ecbb3e4b
evillaz <evillaz123@gmail.com>
2023-03-16 01:20:06 -0500

    update dist

--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -1,5 +1,5 @@
 const addScore = (nameInput, scoreInput) => {
-  const scores = JSON.parse(localStorage.getItem('SCORES')) || [];
+  const scores = JSON.parse(localStorage.getItem('SCORES'));
   const score = { name: nameInput, score: scoreInput };
   scores.push(score);
   localStorage.setItem('SCORES', JSON.stringify(scores));
