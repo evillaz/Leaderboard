e24f84b2cf07020135f4bb10a0be206e19a313f9
evillaz <evillaz123@gmail.com>
2023-03-15 20:26:21 -0500

    update dynamically created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css';
+
+localStorage.setItem('SCORES', '[]');
+
+const addScore = (nameInput, scoreInput) => {
+  const scores = JSON.parse(localStorage.getItem('SCORES')) || [];
+  const score = { name: nameInput, score: scoreInput };
+  scores.push(score);
+  localStorage.setItem('SCORES', JSON.stringify(scores));
+  const listItem = document.createElement('li');
+  listItem.className = 'score';
+  listItem.innerHTML = `${nameInput}: ${scoreInput}`;
+  const list = document.getElementById('scores');
+  list.appendChild(listItem);
+};
+
+addScore('Name', '150');
+addScore('Name', '150');
+addScore('Name', '150');
+addScore('Name', '150');
+addScore('Name', '150');
